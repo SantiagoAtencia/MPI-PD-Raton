6b9a2dd7b2511e019df613959e1934b1ce142b83
SantiagoAtencia <125252149+SantiagoAtencia@users.noreply.github.com>
2024-12-29 22:14:04 +0100

    Delete include/maze_corrector.h

--- include/maze_corrector.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAZE_CORRECTOR_H
-#define MAZE_CORRECTOR_H
-
-#include "maze.h"
-
-// Corrige un laberinto según las reglas especificadas
-void correct_maze(Maze* m);
-
-#endif // MAZE_CORRECTOR_H
